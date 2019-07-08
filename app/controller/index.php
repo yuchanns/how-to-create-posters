c004d4b131a00f0806dacd7a4211a9c83aedbfa6
yuchanns <airamusume@gmail.com>
2019-07-08 21:06:32 +0800

    another creatposters

--- app/controller/index.php
+++ app/controller/index.php
@@ -23,6 +23,12 @@ class Posters
         return file_get_contents($this->dir . '/' . $relative_path);
     }
 
+    /**
+     * 在原背景图上插入名片
+     *
+     * @param [string] $path 图片路径
+     * @return void
+     */
     private function createPoster($path)
     {
         // 实例化图像处理类
@@ -45,12 +51,6 @@ class Posters
         $qr->resize(70, null, $constraint);
         // 对缩放后的二维码进行锐化操作
         $qr->sharpen(15);
-        // 处理海报
-        $poster_path = 'public/image/upload/posters.jpeg';
-        $poster = $manager->make($poster_path);
-        $poster->resize(375, null, $constraint);
-        // 获取海报高度，用与图片合成
-        $poster_height = $poster->height();
         // 处理名片信息（头像、名称、邀请码）
         $username = '羽毛';
         $invitation = '138*****223';
@@ -75,27 +75,33 @@ class Posters
             $font->file('public/image/font/pingfang.ttf');
             $font->size(14);
         };
-        // 创建空白画布
-        // 530
-        $canvas = $manager->canvas(375, 120 + $poster_height, '#fff');
-        $canvas->insert($poster, 'top-left');
-        $canvas->insert($marker, 'top-left', 20, 20);
-        $canvas->insert($qr, 'top-left', 271, $poster_height + 12);
-        $canvas->text('扫码加入', 275, $poster_height + 105, $font);
-        $canvas->insert($avatar, 'top-left', 22, $poster_height + 22);
-        $canvas->text($username, 72, $poster_height + 48, function ($font) {
+        // 创建空白画布，作为名片背景
+        $canvas = $manager->canvas(375, 120, '#fff');
+        // $canvas->insert($poster, 'top-left');
+        // $canvas->insert($marker, 'top-left', 20, 20);
+        $canvas->insert($qr, 'top-left', 271, 12);
+        $canvas->text('扫码加入', 275, 105, $font);
+        $canvas->insert($avatar, 'top-left', 22, 22);
+        $canvas->text($username, 72, 48, function ($font) {
             /**
              * @var \Intervention\Image\AbstractFont $font
              */
             $font->file('public/image/font/pingfang.ttf');
             $font->size(20);
         });
-        $canvas->text('邀请您加入“工作室”', 22, $poster_height + 85, $font);
-        $canvas->text('邀请码：' . $invitation, 22, $poster_height + 105, $font);
-        $canvas->circle(20, 22, 552);
+        $canvas->text('邀请您加入“工作室”', 22, 85, $font);
+        $canvas->text('邀请码：' . $invitation, 22, 105, $font);
+        // 处理海报
+        $poster_path = 'public/image/upload/posters.jpeg';
+        $poster = $manager->make($poster_path);
+        $poster->resize(375, null, $constraint);
+        // 在海报上插入水印
+        $poster->insert($marker, 'top-left', 20, 20);
+        // 在海报上插入名片
+        $poster->insert($canvas, 'bottom-left', 0, 0);
         // 保存为渐进式jpeg
-        $canvas->interlace(true);
-        $canvas->save($path, 80);
+        $poster->interlace(true);
+        $poster->save($path, 80);
     }
 
     private function setQrcode($path)
